2f3866989bf441c2434fea930e1ca1d759a73df7
Yuko Nara <narayuko@gmail.com>
2026-05-13 11:36:25 +0900

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: "numphoto",
-  description: "写真心理学を用いた企業研修・撮影",
+  title: "株式会社ナムフォト | 写真心理学を用いた企業研修 | ブランド撮影",
+  description: "写真を使ったチームビルディング研修を提供。創造的な対話を通じて組織開発や相互理解、メタ認知を促進します。写真心理学では、撮影者の価値観を紐解きます。実績：TIS、NEC、京都リサーチパーク等、2000名以上に提供。",
 };
 
 export default function RootLayout({
